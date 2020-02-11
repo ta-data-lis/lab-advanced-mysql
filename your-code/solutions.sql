002fa7e29dc8bb6b14490984be4dd4b113931feb
duarte cruz <cruz.duarte@gmail.com>
2020-02-11 17:08:41 +0000

    [lab-advanced-mysql] Duarte Cruz
    Is this what's being asked?

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -9,7 +9,7 @@ INNER JOIN AUTHORS ON authors.au_id = titleauthor.au_id
 INNER JOIN titles ON titles.title_id = titleauthor.title_id
 INNER JOIN sales ON titles.title_id = sales.title_id;
 
-# From Step 1 output as a temp table, aggregate the total royalties for each title for each author.
+# From Step 1 output as a derived table, aggregate the total royalties for each title for each author.
 SELECT titles.title_id, authors.au_id, SUM(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS 'royalties'
 FROM titleauthor
 INNER JOIN AUTHORS ON authors.au_id = titleauthor.au_id
@@ -17,7 +17,7 @@ INNER JOIN titles ON titles.title_id = titleauthor.title_id
 INNER JOIN sales ON titles.title_id = sales.title_id
 GROUP BY titles.title_id, authors.au_id;
 
-# Using the output from Step 2 as a temp table, calculate the total profits of each author by aggregating the advances and total royalties of each title.
+# Using the output from Step 2 as a derived table, calculate the total profits of each author by aggregating the advances and total royalties of each title.
 SELECT authors.au_id, SUM((titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) + titles.advance) AS 'profits'
 FROM titleauthor
 INNER JOIN AUTHORS ON authors.au_id = titleauthor.au_id
@@ -29,9 +29,39 @@ LIMIT 3;
 
 # Challenge 2 - Alternative Solution
 
-# I did not understand what would be the alternative solution if not moving the summary into
-# a subquery
+# From Step 1 (in Challenge 1) output as a temp table, aggregate the total royalties for each title for each author.
+CREATE TEMPORARY TABLE temp1 SELECT * FROM titleauthor;
+
+SELECT titles.title_id, authors.au_id, SUM(titles.price * sales.qty * titles.royalty / 100 * temp1.royaltyper / 100)
+FROM temp1
+INNER JOIN AUTHORS ON authors.au_id = temp1.au_id
+INNER JOIN titles ON titles.title_id = temp1.title_id
+INNER JOIN sales ON titles.title_id = sales.title_id
+GROUP BY titles.title_id, authors.au_id;
+
+# Using the output from Step 1 as a temp table, calculate the total profits of each author by aggregating the advances and total royalties of each title.
+CREATE TEMPORARY TABLE temp2 SELECT * FROM titleauthor;
+
+SELECT authors.au_id, SUM((titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) + titles.advance)
+FROM titleauthor
+INNER JOIN AUTHORS ON authors.au_id = titleauthor.au_id
+INNER JOIN titles ON titles.title_id = titleauthor.title_id
+INNER JOIN sales ON titles.title_id = sales.title_id
+GROUP BY titles.title_id, authors.au_id
+ORDER BY SUM((titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) + titles.advance) DESC
+LIMIT 3;
+
 
 # Challenge 3
 
-# I'm lost.
+CREATE TABLE most_profiting_authors SELECT authors.au_id, SUM((titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) + titles.advance) AS 'profits'
+FROM titleauthor
+INNER JOIN AUTHORS ON authors.au_id = titleauthor.au_id
+INNER JOIN titles ON titles.title_id = titleauthor.title_id
+INNER JOIN sales ON titles.title_id = sales.title_id
+GROUP BY titles.title_id, authors.au_id
+ORDER BY SUM((titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) + titles.advance) DESC
+LIMIT 3;
+
+SELECT *
+FROM most_profiting_authors;

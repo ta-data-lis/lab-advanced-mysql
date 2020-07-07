6f95e6b9bd96d71e62226c467a47a4b100baffff
jonnyteronni <antero.soeiro@gmail.com>
2020-07-07 12:45:37 +0100

    Update for solutions for Lab version 2

--- solutions.sql
+++ solutions.sql
@@ -3,6 +3,7 @@ WITH
 	t1 AS (
 		SELECT tt.title_id,
 		ta.au_id,
+		tt.advance,
 		tt.price * sa.qty * tt.royalty / 100 * ta.royaltyper / 100 AS sales_royalties
 		FROM publications.titles AS tt
 		INNER JOIN publications.sales AS sa
@@ -11,44 +12,47 @@ WITH
 			ON tt.title_id = ta.title_id),
 		
 	t2 AS (	
-		SELECT t1.title_id, t1.au_id, sum(t1.sales_royalties) AS total_sales
+		SELECT t1.advance, t1.title_id, t1.au_id, sum(t1.sales_royalties) AS total_sales
 		FROM t1
-		GROUP BY t1.title_id, t1.au_id)
-
-SELECT t2.au_id, sum(t2.total_sales) AS total_sales
+		GROUP BY t1.advance,t1.title_id, t1.au_id)
+	
+		
+SELECT t2.au_id, sum(t2.advance) + sum(t2.total_sales) AS profit
 FROM t2
 GROUP BY t2.au_id
-ORDER BY total_sales DESC
+ORDER BY profit DESC
 lIMIT 3;
 	
 
 /* Challenge 2*/
-SELECT t2.au_id, sum(t2.total_sales) AS total_sales
+SELECT t2.au_id, sum(advance) + sum(t2.total_sales) AS profit
 FROM (	
-	SELECT t1.title_id, t1.au_id, sum(t1.sales_royalties) AS total_sales
+	SELECT t1.advance, t1.title_id, t1.au_id, sum(t1.sales_royalties) AS total_sales
 	FROM (
 		SELECT tt.title_id,
 		ta.au_id,
+		tt.advance,
 		tt.price * sa.qty * tt.royalty / 100 * ta.royaltyper / 100 AS sales_royalties
 		FROM publications.titles AS tt
 		INNER JOIN publications.sales AS sa
 			ON tt.title_id = sa.title_id
 		INNER JOIN publications.titleauthor AS ta
 			ON tt.title_id = ta.title_id) AS t1
-	GROUP BY t1.title_id, t1.au_id) AS t2
+	GROUP BY t1.advance, t1.title_id, t1.au_id) AS t2
 GROUP BY t2.au_id
-ORDER BY total_sales DESC
+ORDER BY profit DESC
 lIMIT 3;
 
 
 /*Challenge 3*/
 CREATE TABLE publications.most_profiting_authors AS
-SELECT temp.au_id, temp.total_sales AS profits
+SELECT temp.au_id, temp.profit AS profits
 FROM (
 		WITH
 		t1 AS (
 			SELECT tt.title_id,
 			ta.au_id,
+			tt.advance,
 			tt.price * sa.qty * tt.royalty / 100 * ta.royaltyper / 100 AS sales_royalties
 			FROM publications.titles AS tt
 			INNER JOIN publications.sales AS sa
@@ -57,12 +61,12 @@ FROM (
 				ON tt.title_id = ta.title_id),
 			
 		t2 AS (	
-			SELECT t1.title_id, t1.au_id, sum(t1.sales_royalties) AS total_sales
+			SELECT t1.advance, t1.title_id, t1.au_id, sum(t1.sales_royalties) AS total_royalties
 			FROM t1
-			GROUP BY t1.title_id, t1.au_id)
+			GROUP BY t1.advance, t1.title_id, t1.au_id)
 	
-	SELECT t2.au_id, sum(t2.total_sales) AS total_sales
+	SELECT t2.au_id, sum(t2.advance) + sum(t2.total_royalties) AS profit
 	FROM t2
 	GROUP BY t2.au_id
-	ORDER BY total_sales DESC) AS temp;
+	ORDER BY profit DESC) AS temp;
 
